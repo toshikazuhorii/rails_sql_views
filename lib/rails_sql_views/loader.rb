8b3c52c45688b9f5c5e818f5aae75541b364215b
Lari Kirby <larikirby@fastmail.fm>
2011-06-09 19:31:08 -0400

    diagnostics

--- lib/rails_sql_views/loader.rb
+++ lib/rails_sql_views/loader.rb
@@ -7,6 +7,9 @@ module RailsSqlViews
       SUPPORTED_ADAPTERS.each do |db|
         if ActiveRecord::ConnectionAdapters.const_defined?("#{db}Adapter")
           require "rails_sql_views/connection_adapters/#{db.downcase}_adapter"
+      puts "\nload_extensions"
+      klass = ActiveRecord::ConnectionAdapters.const_get("#{db}Adapter")
+      puts klass.method_defined?(:tables), klass.method_defined?(:tables_with_views_included)
           ActiveRecord::ConnectionAdapters.const_get("#{db}Adapter").class_eval do
             include RailsSqlViews::ConnectionAdapters::AbstractAdapter
             include RailsSqlViews::ConnectionAdapters.const_get("#{db}Adapter")
